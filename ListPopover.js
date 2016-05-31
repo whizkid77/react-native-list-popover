a3a2bbb6910e35d0642a5f39da30fe7cb98b5975
James Lin <james77@gmail.com>
2016-05-30 20:32:30 -0700

    Update to support react native > 0.26 React style imports

--- ListPopover.js
+++ ListPopover.js
@@ -3,16 +3,19 @@
  */
 "use strict";
 
-var React = require('react-native');
-var {
-  ListView,
-  PropTypes,
-  StyleSheet,
-  Text,
-  Dimensions,
-  TouchableOpacity,
-  View
-} = React;
+import React, {
+    PropTypes,
+} from 'react';
+
+import {
+    ListView,
+    StyleSheet,
+    Text,
+    Dimensions,
+    TouchableOpacity,
+    View
+} from 'react-native';
+
 var SCREEN_HEIGHT = Dimensions.get('window').height;
 var noop = () => {};
 var ds = new ListView.DataSource({rowHasChanged: (r1,r2)=>(r1!==r2)});
